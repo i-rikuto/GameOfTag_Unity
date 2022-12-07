a79f9cc57d71e5cd41fe32084f8a670508177e30
r-izume <izume.rikuto@gmail.com>
2022-12-07 16:01:18 +0900

    Testスクリプトの追加

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        hoge();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void hoge()
+    {
+        Debug.Log("hoge");
+    }
+}
