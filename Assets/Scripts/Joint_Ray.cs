be136eca7638639f880179e1c0868aa2175df96b
r-izume <izume.rikuto@gmail.com>
2022-12-09 18:28:53 +0900

    照準機能の追加

---
+++ Assets/Scripts/Joint_Ray.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joint_Ray : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Front,RayEnd,RayError;
+    public Material RayMaterial;
+    [SerializeField] LineRenderer line;
+    public float rotationSpeed = 1000.0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                Front.transform.LookAt(hit.point);
+                Debug.Log(hit.collider.gameObject.transform.position);
+            }
+        }
+        
+        float step = rotationSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.RotateTowards(this.transform.rotation, Front.transform.rotation, step);
+        this.transform.rotation = rotation;
+        line.SetPosition(0, this.transform.position);
+        line.SetPosition(1, RayEnd.transform.localPosition );
+        RaycastHit _hit;
+        if (Physics.Raycast(this.gameObject.transform.position, transform.forward,out _hit,1000)) {
+            RayEnd.transform.position = _hit.point;
+            RayMaterial.color = Color.blue;
+        }
+        else
+        {
+            RayEnd.transform.position = RayError.transform.position;
+            RayMaterial.color = Color.red;
+        }
+        
+        //target = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, mouse.z));
+        //transform.LookAt(target);
+    }
+}
